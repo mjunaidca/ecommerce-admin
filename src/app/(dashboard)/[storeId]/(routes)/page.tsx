6a4027d6bea1156d1807290d06faa9e794395702
Muhammad Junaid <mr.junaidshaukat@gmail.com>
2023-08-15 19:44:33 +0500

    feat complete Navigation bar

--- src/app/(dashboard)/[storeId]/(routes)/page.tsx
+++ src/app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,5 +1,16 @@
-const DashboardPage = () => {
-  return <div>DashboardPage</div>;
+import prismadb from "@/lib/prismadb";
+
+interface DasahboardPageProps {
+  params: { storeId: string };
+}
+
+const DashboardPage: React.FC<DasahboardPageProps> = async ({ params }) => {
+  const store = await prismadb.store.findFirst({
+    where: {
+      id: params.storeId,
+    },
+  });
+  return <div>Active Stpre: {store?.name}</div>;
 };
 
 export default DashboardPage;
