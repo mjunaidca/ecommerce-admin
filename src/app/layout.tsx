bdbf3a887c8883307859e6b20ec4313835e36076
Muhammad Junaid <mr.junaidshaukat@gmail.com>
2023-08-17 18:47:18 +0500

    product entity completed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Admin Dasahboard",
-  description: "Admin Dasahboard",
+  description: "Admin Dashboard",
 };
 
 export default function RootLayout({
